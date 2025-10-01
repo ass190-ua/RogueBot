aa9052fa469e93390873636ff6f902ffc78e706d
Arturo Soriano Sánchez <78472000+ass190-ua@users.noreply.github.com>
2025-10-01 16:13:21 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  std::cout << "RogueBot Alpha (placeholder)\n";
+  std::cout << "Simulando run -> muerte -> Game Over.\n";
+  std::cout << "Pulsa ENTER para reiniciar (placeholder).\n";
+  std::cin.get();
+  return 0;
+}
