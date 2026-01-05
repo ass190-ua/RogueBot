8227a7d7959516e5a0b9512d0ed218de792a9032
rla28-ua <rla28@alu.ua.es>
2026-01-05 11:34:16 +0100

    ci: desactivar i18n/gettext en Windows y excluir tests de gettext

--- src/core/I18n.hpp
+++ src/core/I18n.hpp
@@ -1,14 +1,7 @@
 #pragma once
 
-// Helpers para GNU gettext (i18n)
-#include <libintl.h>
+#define _(String) (String)
 
-// Traduce en runtime
-#ifndef _
-#define _(String) gettext(String)
-#endif
-
-// Marca para traducir, pero NO traduce aún (útil para tablas estáticas)
 #ifndef N_
 #define N_(String) (String)
 #endif
