0364c48374b747bf6f561ef8d75c6e6bb93ece62
rla28-ua <rla28@alu.ua.es>
2026-01-05 11:46:22 +0100

    workflow actualizado con Linux/Windows matrix

--- src/core/I18n.hpp
+++ src/core/I18n.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
-#define _(String) (String)
+// Helpers para GNU gettext (i18n)
+#include <libintl.h>
 
+// Traduce en runtime
+#ifndef _
+#define _(String) gettext(String)
+#endif
+
+// Marca para traducir, pero NO traduce aún (útil para tablas estáticas)
 #ifndef N_
 #define N_(String) (String)
 #endif
