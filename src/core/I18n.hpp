978784817d11338468b54f17d75283576e191fc6
rla28-ua <78477239+rla28-ua@users.noreply.github.com>
2026-01-05 13:37:30 +0100

    CI matrix (Linux/Windows) y unificación de workflows (#124)
    * ci: preparar matrix Linux/Windows en el workflow principal
    
    * ci: arreglar Boost en Windows
    
    * ci: desactivar gettext en Windows para permitir build y tests unit
    
    * Vuelta atrás
    
    * ci: desactivar gettext en Windows para permitir build y tests unit
    
    * ci: hacer Intl opcional y desactivar i18n en Windows
    
    * ci: arreglar build Windows (PowerShell) y detección de Boost con vcpkg
    
    * ci: cachear vcpkg en Windows para acelerar la instalación de Boost
    
    * ci: desactivar i18n/gettext en Windows y excluir tests de gettext
    
    * workflow actualizado con Linux/Windows matrix
    
    * corregir baseline de vcpkg en Windows
    
    * activar caché de vcpkg en Windows
    
    * instalar gettext en Windows para compilar i18n
    
    * reducir dependencias en Windows para acelerar el workflow
    
    * permitir compilar sin libintl cuando i18n está desactivado
    
    * desactivar gettext cuando i18n está apagado
    
    * activar traducciones en builds Makefile cuando existe gettext

--- src/core/I18n.hpp
+++ src/core/I18n.hpp
@@ -1,14 +1,41 @@
 #pragma once
 
-// Helpers para GNU gettext (i18n)
-#include <libintl.h>
+// Si CMake define RB_ENABLE_I18N, obedecemos.
+#if defined(RB_ENABLE_I18N)
 
-// Traduce en runtime
+#if RB_ENABLE_I18N
+#include <libintl.h>
 #ifndef _
 #define _(String) gettext(String)
 #endif
+#else
+#ifndef _
+#define _(String) (String)
+#endif
+#endif
 
-// Marca para traducir, pero NO traduce aún (útil para tablas estáticas)
+// Si NO está definido (caso Makefile), autodetectamos.
+#else
+
+#if defined(__has_include)
+#if __has_include(<libintl.h>)
+#include <libintl.h>
+#ifndef _
+#define _(String) gettext(String)
+#endif
+#else
+#ifndef _
+#define _(String) (String)
+#endif
+#endif
+#else
+#ifndef _
+#define _(String) (String)
+#endif
+#endif
+
+#endif
+
 #ifndef N_
 #define N_(String) (String)
 #endif
